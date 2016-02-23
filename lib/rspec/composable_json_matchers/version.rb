77cfb084da7bdc11820560afbb8957394f74981e
Yuji Nakayama <nkymyj@gmail.com>
2016-02-23 18:39:34 +0900

    Bump version to 1.0.0

--- lib/rspec/composable_json_matchers/version.rb
+++ lib/rspec/composable_json_matchers/version.rb
@@ -2,8 +2,8 @@ module RSpec
   module ComposableJSONMatchers
     # @api public
     module Version
-      MAJOR = 0
-      MINOR = 1
+      MAJOR = 1
+      MINOR = 0
       PATCH = 0
 
       def self.to_s
